c2804b8d0703e330465e9082ad89eba8a93e94da
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2024-05-16 12:10:07 +0530

    Update JumboSocketSettings.hpp

--- JumboSocketSettings.hpp
+++ JumboSocketSettings.hpp
@@ -70,3 +70,40 @@ int create_jumbo_tcp_socket()
 
     return sock;
 }
+
+int create_jumbo_sctp_socket() 
+{
+    int sock_fd;
+    struct sockaddr_in servaddr;
+    struct sctp_initmsg initmsg;
+    int val;
+
+    // Create an SCTP socket
+    sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    if (sock_fd < 0) 
+    {
+        perror("socket");
+        return -1;
+    }
+
+    // Set SCTP socket options for Jumbo frames
+    memset(&initmsg, 0, sizeof(initmsg));
+    initmsg.sinit_num_ostreams = 1;
+    initmsg.sinit_max_instreams = 1;
+    initmsg.sinit_max_attempts = 4;
+    val = 65535; // Set Jumbo frame size
+    if (setsockopt(sock_fd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg)) < 0) 
+    {
+        perror("setsockopt SCTP_INITMSG");
+        close(sock_fd);
+        return -1;
+    }
+    if (setsockopt(sock_fd, IPPROTO_SCTP, SCTP_MAXSEG, &val, sizeof(val)) < 0) 
+    {
+        perror("setsockopt SCTP_MAXSEG");
+        close(sock_fd);
+        return -1;
+    }
+
+   return sock_fd;
+}
